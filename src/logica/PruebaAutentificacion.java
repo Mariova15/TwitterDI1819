4f3d34377df6c3a0a6363445b00c4851dfc053cc
Shaila <shailaperezfernandez@gmail.com>
2019-01-13 11:42:23 +0100

    Actualizar tu estado

--- src/logica/PruebaAutentificacion.java
+++ src/logica/PruebaAutentificacion.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package logica;
 
 import java.awt.Desktop;
@@ -29,38 +24,45 @@ import twitter4j.auth.RequestToken;
  * @author adan
  */
 public class PruebaAutentificacion {
-    
+
     public static void main(String[] args) throws TwitterException, IOException, URISyntaxException, InterruptedException {
         //añadimos los token de nuestra aplicacion
         Twitter twitter = TwitterFactory.getSingleton();
         twitter.setOAuthConsumer("HkM0LS2MQLnh2c4ECo40HHYmP", "uPhHLyAJahCC4ui9nC2AHvzYZEEGDnNyoshKsXIL845C3MVFWG"); //poco seguro
-        
+
         //pedimos un login
         RequestToken request = twitter.getOAuthRequestToken();
         Desktop.getDesktop().browse(new URI(request.getAuthorizationURL())); //esto lanza el navegador por defecto con la pagina de login
-        
-        System.out.println("introduce el pin: "); //se introduce el pin
+
+        System.out.println("Introduce el pin: "); //se introduce el pin
         String pin = new Scanner(System.in).nextLine();
-        AccessToken access= null;
-        try{
-        access = twitter.getOAuthAccessToken(request, pin);
-        }catch(TwitterException te){ //comprobamos errores
-            if(te.getStatusCode()==401) System.out.println("Unable to get the access token");
-            else te.printStackTrace();
+        AccessToken access = null;
+        try {
+            access = twitter.getOAuthAccessToken(request, pin);
+        } catch (TwitterException te) { //comprobamos errores
+            if (te.getStatusCode() == 401) {
+                System.out.println("Unable to get the access token");
+            } else {
+                te.printStackTrace();
+            }
             System.exit(-1); //salimos de la aplicación si hay
         }
-        
+
         System.out.println("acceso con éxito");
-        
+
         twitter.setOAuthAccessToken(access); //añadimos el access token a la aplicacion
+
+        //Actualizar tu estado
+        Status tweetEscrito = twitter.updateStatus("Prueba tweetEscrito.");
         
-        while(true){
+        while (true) {
             for (Status status : twitter.getHomeTimeline()) {
-                System.out.println(status.getUser().getName()+" : "+status.getText()); //timeline
+                System.out.println(status.getUser().getName() + " : " + status.getText()); //timeline
+
             }
             Thread.sleep(10000); //cada 10 segundos se recarga
         }
-        
+
     }
-    
+
 }
